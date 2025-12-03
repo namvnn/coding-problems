504fb691747749cb5b7bdf7b3621f28f26115f39
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-12-03 11:33:38 +1100

    aoc(cpp): 2025/day-2/part-2

---
+++ advent-of-code/cpp/2025/day-02/part-2.cpp
@@ -0,0 +1,83 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#define ll long long
+#define newl '\n'
+
+using namespace std;
+
+bool is_invalid(string id) {
+  int len = id.length();
+
+  for (int pattern_len = 1; pattern_len < len; pattern_len++) {
+    if (len % pattern_len != 0) {
+      continue;
+    }
+
+    int pattern_count = len / pattern_len;
+    for (int i = 1; i < pattern_count; i++) {
+      string first = id.substr((i - 1) * pattern_len, pattern_len);
+      string second = id.substr(i * pattern_len, pattern_len);
+      if (first != second) {
+        break;
+      }
+      if (i == pattern_count - 1) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+long long convert_str_long(string num) {
+  long long r = 0;
+  int len = num.length();
+  for (int i = 0; i < len; i++) {
+    r += (num[len - i - 1] - '0') * pow(10, i);
+  }
+  return r;
+}
+
+int main() {
+  string filename = "input.txt";
+  ifstream file(filename);
+
+  if (!file.is_open()) {
+    cout << "error: " << filename << " does not exist" << newl;
+    return 1;
+  }
+
+  string line;
+  long long sum = 0;
+
+  while (getline(file, line)) {
+    string range;
+    stringstream range_ss(line);
+
+    while (getline(range_ss, range, ',')) {
+      string first_str;
+      string second_str;
+      stringstream id_ss(range);
+
+      getline(id_ss, first_str, '-');
+      getline(id_ss, second_str, '-');
+
+      long long first = convert_str_long(first_str);
+      long long second = convert_str_long(second_str);
+
+      for (long long id = first; id <= second; id++) {
+        if (is_invalid(to_string(id))) {
+          // cout << "Found Invalid ID : " << id << newl;
+          sum += id;
+        }
+      }
+    }
+  }
+
+  cout << sum << newl;
+
+  file.close();
+}
