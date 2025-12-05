6747d1ac5f106cb6166227bfb7e754ce6457acaf
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-12-05 21:05:30 +1100

    aoc(2025): cpp/day-3/part-1

---
+++ advent-of-code/cpp/2025/day-03/part-1.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#define ll long long
+#define newl '\n'
+
+using namespace std;
+
+int main() {
+  string filename = "input.txt";
+  ifstream file(filename);
+
+  if (!file.is_open()) {
+    cout << "error: " << filename << " does not exist" << newl;
+    return 1;
+  }
+
+  string bank;
+  int sum = 0;
+
+  while (getline(file, bank)) {
+    int len = bank.length();
+    int largest = 0;
+
+    for (int i = 0; i < len - 1; i++) {
+      for (int j = i + 1; j < len; j++) {
+        int num = (bank[i] - '0') * 10 + (bank[j] - '0');
+        largest = max(largest, num);
+      }
+    }
+
+    sum += largest;
+  }
+
+  cout << sum << newl;
+
+  file.close();
+}
