8c4ecb78dde2ae906ca6aa445f76f3986ad31ad5
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-12-07 21:17:44 +1100

    aoc(2025): imporve cpp/day-3/part-1

--- advent-of-code/cpp/2025/day-03/part-1.cpp
+++ advent-of-code/cpp/2025/day-03/part-1.cpp
@@ -7,6 +7,20 @@
 
 using namespace std;
 
+int find_largest_joltage(string bank) {
+  int len = bank.length();
+  int largest = 0;
+
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      int num = (bank[i] - '0') * 10 + (bank[j] - '0');
+      largest = max(largest, num);
+    }
+  }
+
+  return largest;
+}
+
 int main() {
   string filename = "input.txt";
   ifstream file(filename);
@@ -20,17 +34,7 @@ int main() {
   int sum = 0;
 
   while (getline(file, bank)) {
-    int len = bank.length();
-    int largest = 0;
-
-    for (int i = 0; i < len - 1; i++) {
-      for (int j = i + 1; j < len; j++) {
-        int num = (bank[i] - '0') * 10 + (bank[j] - '0');
-        largest = max(largest, num);
-      }
-    }
-
-    sum += largest;
+    sum += find_largest_joltage(bank);
   }
 
   cout << sum << newl;
